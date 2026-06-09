9557f86e7c06155712317b3e1d79c5c7c4da4ba0
Dhairya Kumar <gdhairya29@gmail.com>
2026-06-09 21:25:54 +0530

    Remove unnecessary SQL files and lock files

--- update_password_krishna.sql
+++
@@ -1,1 +0,0 @@
-UPDATE users SET password_hash = 'pbkdf2$81d2ab1b05216ade243cc5d0282265e6$100000$cd22aa10e97f1105ccd00d3e41a546bfc8fd71c5e9e54b292a474f487eeb49ab' WHERE email = 'krishnagaming99@gmail.com';
